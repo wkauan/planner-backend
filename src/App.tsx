9dc05bd21e8dfee0fc1cdef355255021a8a93a19
wkauan <kauanmbomfim5@gmail.com>
2024-07-09 01:28:08 -0300

    feat: Criando função para remover o email dos convidados

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,14 @@ export function App() {
     event.currentTarget.reset();
   }
 
+  function removeEmailToInvite(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove
+    );
+
+    setEmailsToInvite(newEmailList);
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -170,7 +178,10 @@ export function App() {
                     className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
                   >
                     <span className="text-zinc-300">{email}</span>
-                    <button type="button">
+                    <button
+                      type="button"
+                      onClick={() => removeEmailToInvite(email)}
+                    >
                       <X className="size-4 text-zinc-400" />
                     </button>
                   </div>
