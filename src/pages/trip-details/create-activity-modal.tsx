3cb981353f9fc20f75c2cb1dab94e6a6ef4e50b3
wkauan <kauanmbomfim5@gmail.com>
2024-07-09 17:18:12 -0300

    feat: Atualizando o botão para o componente

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,4 +1,5 @@
 import { Calendar, Tag, X } from 'lucide-react';
+import { Button } from '../../components/button';
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void;
@@ -45,12 +46,7 @@ export function CreateActivityModal({
             </div>
           </div>
 
-          <button
-            type="submit"
-            className="w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
-          >
-            Confirmar criação da viagem
-          </button>
+          <Button size="full">Salvar atividade</Button>
         </form>
       </div>
     </div>
--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -5,6 +5,7 @@ import { ImportantLinks } from './important-links';
 import { Guests } from './guests';
 import { Activities } from './activities';
 import { DestinationAndDateHeader } from './destination-and-date-header';
+import { Button } from '../../components/button';
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -27,13 +28,10 @@ export function TripDetailsPage() {
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
 
-            <button
-              onClick={openCreateActivityModal}
-              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-            >
+            <Button onClick={openCreateActivityModal}>
               <Plus className="size-5" />
               Cadastrar atividades
-            </button>
+            </Button>
           </div>
 
           <Activities />
