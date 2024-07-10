ae5d660ad937b487f56bb10573de61cf116e3d5d
wkauan <kauanmbomfim5@gmail.com>
2024-07-09 21:18:38 -0300

    feat: Registrando rota

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { confirmTrp } from "./routes/confirm-trip";
 
 const app = fastify();
 
@@ -11,6 +12,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrp);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running!");
