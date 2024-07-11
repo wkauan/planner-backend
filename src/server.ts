4945aa1bb85c3bdf0b836c47ac569799c57bab03
wkauan <kauanmbomfim5@gmail.com>
2024-07-11 11:29:11 -0300

    feat: Criando rota para mostrar os detalhes da viagem

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { getActivities } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
 
 const app = fastify();
 
@@ -34,6 +35,7 @@ app.register(getLinks);
 app.register(getParticipants);
 app.register(createInvite);
 app.register(updateTrip);
+app.register(getTripDetails);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running!");
