e52c997794d24c0fd456634ff5e9793ab3873a31
wkauan <kauanmbomfim5@gmail.com>
2024-07-11 11:23:01 -0300

    feat: Criando rota para atualizar detalhes da viagem

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ import { createTrip } from "./routes/create-trip";
 import { getActivities } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { updateTrip } from "./routes/update-trip";
 
 const app = fastify();
 
@@ -32,6 +33,7 @@ app.register(createLink);
 app.register(getLinks);
 app.register(getParticipants);
 app.register(createInvite);
+app.register(updateTrip);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running!");
